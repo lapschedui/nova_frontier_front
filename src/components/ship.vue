0c93dfba716f3ec4fb399b62256c8fa04eda6ccd
l-pogorelchenko <l.pogorelchenko@m3ii.tech>
2026-09-20 21:25:39 +0600

    fix dinamic path

--- src/components/ship.vue
+++ src/components/ship.vue
@@ -15,13 +15,13 @@ function finishJump() {
 }
 
 function moveToHangar() {
-    currentBg.value = new URL('/images/hangar/' + localStorage.now + '.png', import.meta.url).href;
+    currentBg.value = new URL(window.location.origin + import.meta.env.BASE_URL + '/images/hangar/' + localStorage.now + '.png', import.meta.url).href;
     disableTransition.value = true;
     isZooming.value = false;
 }
 
 function changeBackground() {
-    currentBg.value = new URL('/images/hyperDrive/finish/' + localStorage.finish + '.png', import.meta.url).href;
+    currentBg.value = new URL(window.location.origin + import.meta.env.BASE_URL + '/images/hyperDrive/finish/' + localStorage.finish + '.png', import.meta.url).href;
     isZooming.value = true;
     console.log('change bg to ', localStorage.finish);
     localStorage.now = localStorage.finish;
@@ -33,11 +33,11 @@ function changeBackground() {
 onMounted(() => {
     if (localStorage.start && localStorage.start !== 'undefined') {
         console.log('start is', localStorage.start);
-        currentBg.value = new URL('/images/hyperDrive/start/' + localStorage.start + '.png', import.meta.url).href;
+        currentBg.value = new URL(window.location.origin + import.meta.env.BASE_URL + '/images/hyperDrive/start/' + localStorage.start + '.png', import.meta.url).href;
     } else if (localStorage.now && localStorage.now !== 'undefined') {
-        currentBg.value = new URL('/images/hangar/' + localStorage.now + '.png', import.meta.url).href
+        currentBg.value = new URL(window.location.origin + import.meta.env.BASE_URL + '/images/hangar/' + localStorage.now + '.png', import.meta.url).href
     } else {
-        currentBg.value = new URL('/images/hangar/AstraNexus.png', import.meta.url).href;
+        currentBg.value = new URL(window.location.origin + import.meta.env.BASE_URL + '/images/hangar/AstraNexus.png', import.meta.url).href;
         localStorage.now = 'AstraNexus';
     }
 
